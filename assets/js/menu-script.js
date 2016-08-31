3fba8a4640840bffaabae0514c240e94b7945bcd
Konstantin Stefanov <kosio7@gmail.com>
2016-08-31 14:56:17 +0300

    Style improvements.

--- assets/js/menu-script.js
+++ assets/js/menu-script.js
@@ -1,18 +1,25 @@
-var element = document.getElementById('main-content');
+var element = document.getElementById("main-content");
 element.addEventListener('click', closeNav);
+
+var menuButton = document.getElementById("menuButton");
+
 
 function openNav() {
     document.getElementById("navigation").style.width = "300px";
     document.getElementById("main-content").style.marginLeft = "300px";
     document.getElementById("header-content").style.marginLeft = "300px";
+    document.getElementById("menuButton").style.marginLeft = "300px";
 }
 
 function closeNav() {
     var elWidth = document.getElementById("navigation").style.width;
-
+    
     if (parseInt(elWidth) > 0) {
     	document.getElementById("navigation").style.width = "0";
-    }
+    }    
+
     document.getElementById("main-content").style.marginLeft = "0px";
     document.getElementById("header-content").style.marginLeft = "0px";
+    document.getElementById("menuButton").style.marginLeft = "0px";
+
 }
